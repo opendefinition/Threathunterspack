7fffac04fd8e8257b999383bb3bcf24ffb67ab35
rjohnsen <johnsen@opendefinition.com>
2020-10-20 13:29:36 +0200

    Rewrote plugin for extracting IPv4 addresses

--- ipv4extractbracketedfromtext.py
+++ ipv4extractbracketedfromtext.py
@@ -5,14 +5,26 @@ import re
 
 class Ipv4extractbracketedfromtextCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		document_text = self.view.substr(sublime.Region(0, self.view.size()))
+		document_text = re.sub(
+				r"[\[\]\<\>a-zA-Z]",
+				"",
+				self.view.substr(sublime.Region(0, self.view.size()))
+			)
 
 		if len(document_text) > 0:
-			ipv4_regex = "(\d{1,3}[\[\.\]]{3}\d{1,3}[\[\.\]]{3}\d{1,3}[\[\.\]]{3}\d{1,3})"
-			ipv4_extracted = re.findall(ipv4_regex, document_text)
+			splitted = document_text.split("\n")
+			findings = []
 
-			if len(ipv4_extracted) > 0:
-				out_text = ",\n".join(ipv4_extracted).replace("[", "").replace("]", "")
+			for line in splitted:
+				item = line.replace(" ", "")
 
-				self.view.erase(edit, sublime.Region(0, self.view.size()))
-				self.view.insert(edit, 0, out_text)
+				ipv4_regex = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+				ipv4_extracted = re.findall(ipv4_regex, item)
+
+				if len(ipv4_extracted) > 0:
+					if ipv4_extracted[0] not in findings:
+						findings.append(ipv4_extracted[0])
+
+			out_text = "\n".join(findings)
+			self.view.erase(edit, sublime.Region(0, self.view.size()))
+			self.view.insert(edit, 0, out_text)
