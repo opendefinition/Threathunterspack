06a4335b7b12aebaa8d9d87f2bc7640f9992e5db
rjohnsen <rjohnsen@opendefinition.com>
2022-11-02 11:31:00 +0100

    Added script to make regex from selected text. For Elastic use

---
+++ elasticregexifytext.py
@@ -0,0 +1,45 @@
+import sublime
+import sublime_plugin
+
+class ElasticregexifytextCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sel = self.view.sel()[0]
+		selected = self.view.substr(sel)
+
+		replacements = [
+			".",
+			"^",
+			"$",
+			"*",
+			"+",
+			"-",
+			"?",
+			"(",
+			")",
+			"[",
+			"]",
+			"{",
+			"}",
+			"|"
+		]
+
+		legal = [
+			"_",
+			":",
+			"&"
+		]
+		
+		char_regx = ""
+		for char in selected:
+			if char.isdigit() or char in legal:
+				char_regx += char
+			elif char is " ":
+				char_regx += "\\s"
+			elif char is "\\":
+				char_regx += "\\\\"
+			elif char in replacements:
+				char_regx += "\\{character}".format(character=char)
+			else:
+				char_regx += "[{up}{lo}]".format(lo=char.lower(), up=char.upper())
+
+		self.view.replace(edit, sel, "/.*{chars}.*/".format(chars=char_regx))
